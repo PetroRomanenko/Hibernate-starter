bfb19e33a284dbd2145f4cad337e5d378c7dd109
PetroRomanenko <83536498+PetroRomanenko@users.noreply.github.com>
2023-03-26 16:06:32 +0300

    New chapter Logging

--- src/main/java/com/ferros/HibernateRunner.java
+++ src/main/java/com/ferros/HibernateRunner.java
@@ -4,6 +4,7 @@ import com.ferros.converter.BirthdayConverter;
 import com.ferros.entity.Birthday;
 import com.ferros.entity.Role;
 import com.ferros.entity.User;
+import com.ferros.util.HibernateUtil;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -14,46 +15,26 @@ import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) throws SQLException {
-//        BlockingQueue<Connection> pool = null;
-//        Connection connection=  pull.take
-//        SessionFactory
-//        Connection connection = DriverManager
-//                .getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "dr005764");
-        Configuration configuration = new Configuration();
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-//        configuration.addAnnotatedClass(User.class);
-        configuration.addAttributeConverter(new BirthdayConverter());
-        configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure();
-
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-
-            session.beginTransaction();
-//            User user = User.builder()
-//                    .username("ivan9@gmail.com")
-//                    .firstname("Ivan")
-//                    .lastname("Ivanov")
-//                    .info("""
-//                            {
-//                                "name": "Ivan",
-//                                "id": 25
-//                            }
-//                            """)
-//                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
-//                    .role(Role.ADMIN)
-//                    .build();
-
-            User user1 = session.get(User.class, "ivan2@gmail.com");
-            user1.setLastname("Petrov");
-            session.flush();
+        User user = User.builder()
+                .username("ivan@gmail.com")
+                .lastname("Ivanov")
+                .firstname("Ivan")
+                .build();
 
 
-//            session.evict(user1);
-//            session.clear();
-//            session.close();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()){
+             try(Session session1 = sessionFactory.openSession()) {
+                session1.beginTransaction();
+                session1.save(user);
 
-            session.getTransaction().commit();
+                session1.getTransaction().commit();
+            } try(Session session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
+                user.setFirstname("Sveta");
+//                session2.delete(user);
+                session2.refresh(user);
+                session2.getTransaction().commit();
+            }
         }
 
 
